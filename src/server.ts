ab810989ab19791af8103099dca96588c8e1a809
Roger Correa <136924837+rmeIIo@users.noreply.github.com>
2025-07-14 19:53:26 -0300

    feat: add rooms listing

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
 } from "fastify-type-provider-zod";
 import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -19,5 +20,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.get("/health", () => {
   return "OK";
 });
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT ? Number(process.env.PORT) : 3333 });
